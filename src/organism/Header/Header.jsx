317e52ef36dff0bffc810295e8d41397be5c255d
gegyhamdani <gegyhamdani@gmail.com>
2020-06-17 23:00:11 +0700

    show how much product at header

--- src/organism/Header/Header.jsx
+++ src/organism/Header/Header.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+/* eslint-disable react/jsx-one-expression-per-line */
+import React, { useContext } from 'react';
 import styles from './index.module.css';
+import ProductsContext from '../../util/ProductsContext';
 
 const Header = () => {
-  return <div className={styles.header}>Total Product : X</div>;
+  const { productList } = useContext(ProductsContext);
+
+  return (
+    <>
+      <div className={styles.header}>Total Product: {productList.length}</div>
+    </>
+  );
 };
 
 export default Header;
