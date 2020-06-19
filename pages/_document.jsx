91ecb2ed18db20f1ec773f602ced0998cd6161e2
gegyhamdani <gegyhamdani@gmail.com>
2020-06-19 11:04:04 +0700

    change style global position

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -18,23 +18,6 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <link rel="shortcut icon" href="/favicon.ico" />
           <link rel="apple-touch-icon" href="/favicon.ico" />
-
-          <style jsx global>
-            {`
-              html,
-              body {
-                padding: 0;
-                margin: 0;
-                font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-                  Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
-                  Helvetica Neue, sans-serif;
-              }
-
-              * {
-                box-sizing: border-box;
-              }
-            `}
-          </style>
         </Head>
         <body>
           <Main />
--- src/templates/Pages/Pages.jsx
+++ src/templates/Pages/Pages.jsx
@@ -3,7 +3,27 @@ import PropTypes from 'prop-types';
 import styles from './index.module.css';
 
 const Pages = ({ children }) => {
-  return <div className={styles.container}>{children}</div>;
+  return (
+    <>
+      <div className={styles.container}>{children}</div>
+      <style jsx global>
+        {`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+              sans-serif;
+          }
+
+          * {
+            box-sizing: border-box;
+          }
+        `}
+      </style>
+    </>
+  );
 };
 Pages.propTypes = {
   children: PropTypes.oneOfType([
